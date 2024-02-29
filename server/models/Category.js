3271ca281941996d7784020eda0d0d2cebcfef2a
vivek-panchal <vivek.nsut@gmail.com>
2024-02-29 10:54:41 +0530

    small error fix in category model

--- server/models/Category.js
+++ server/models/Category.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-const tagsSchema = new mongoose.Schema({
+const categorySchema = new mongoose.Schema({
     name:{
         type:String,
         required: true,
@@ -14,4 +14,4 @@ const tagsSchema = new mongoose.Schema({
     },
 });
 
-module.exports = mongoose.model("Tag", tagsSchema);
+module.exports = mongoose.model("Category", categorySchema);

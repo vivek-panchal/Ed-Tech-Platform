eaee34627e359d8798cd1b96c9952911d5ffcfac
vivek-panchal <vivek.nsut@gmail.com>
2024-03-22 11:06:31 +0530

    Razorpay Payment gateway intigration

--- server/routes/Payments.js
+++ server/routes/Payments.js
@@ -2,9 +2,10 @@
 const express = require("express")
 const router = express.Router()
 
-const { capturePayment, verifySignature } = require("../controllers/Payments")
+const { capturePayment, verifyPayment, sendPaymentSuccessEmail } = require("../controllers/Payments")
 const { auth, isInstructor, isStudent, isAdmin } = require("../middlewares/auth")
 router.post("/capturePayment", auth, isStudent, capturePayment)
-router.post("/verifySignature", verifySignature)
+router.post("/verifyPayment",auth, isStudent, verifyPayment)
+router.post("/sendPaymentSuccessEmail", auth, isStudent, sendPaymentSuccessEmail);
 
 module.exports = router
--- src/App.js
+++ src/App.js
@@ -24,6 +24,7 @@ import AddCourse from "./components/core/Dashboard/AddCourse";
 import MyCourses from "./components/core/Dashboard/MyCourses";
 import EditCourse from "./components/core/Dashboard/EditCourse";
 import Catalog from "./pages/Catalog";
+import CourseDetails from "./pages/CourseDetails";
 
 function App() {
 
@@ -38,7 +39,7 @@ function App() {
     <Routes>
       <Route path="/" element={<Home/>} />
       <Route path="catalog/:catalogName" element={<Catalog/>} />
-      
+      <Route path="courses/:courseId" element={<CourseDetails/>} />
       <Route
           path="signup"
           element={

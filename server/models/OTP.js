41eb55a7370e63a1dfb440bc8f59afd69b7096e3
vivek-panchal <vivek.nsut@gmail.com>
2024-03-26 21:23:37 +0530

    Tring to fix error , run smoothly on localhost

--- server/models/OTP.js
+++ server/models/OTP.js
@@ -1,7 +1,6 @@
 const mongoose = require("mongoose");
 const mailSender = require("../utils/mailSender");
 const emailTemplate = require("../mail/templates/emailVerificationTemplate");
-
 const OTPSchema = new mongoose.Schema({
 	email: {
 		type: String,
@@ -31,7 +30,7 @@ async function sendVerificationEmail(email, otp) {
 			"Verification Email",
 			emailTemplate(otp)
 		);
-		console.log("Email sent successfully: ", mailResponse?.response);
+		console.log("Email sent successfully: ", mailResponse.response);
 	} catch (error) {
 		console.log("Error occurred while sending email: ", error);
 		throw error;
@@ -44,7 +43,7 @@ OTPSchema.pre("save", async function (next) {
 
 	// Only send an email when a new document is created
 	if (this.isNew) {
-		await sendVerificationEmail(this?.email, this?.otp);
+		await sendVerificationEmail(this.email, this.otp);
 	}
 	next();
 });

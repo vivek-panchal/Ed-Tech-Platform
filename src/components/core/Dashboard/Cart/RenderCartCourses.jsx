21ba1d0dfec0a435cb85ec4c35056d53f2f8996d
vivek-panchal <vivek.nsut@gmail.com>
2024-03-27 15:02:24 +0530

    card ui update

--- src/components/core/Dashboard/Cart/RenderCartCourses.jsx
+++ src/components/core/Dashboard/Cart/RenderCartCourses.jsx
@@ -21,7 +21,7 @@ export default function RenderCartCourses() {
             <img
               src={course?.thumbnail}
               alt={course?.courseName}
-              className="h-[148px] w-[220px] rounded-lg object-cover"
+              className="h-[120px] w-[220px] rounded-lg object-cover"
             />
             <div className="flex flex-col space-y-1">
               <p className="text-lg font-medium text-richblack-5">

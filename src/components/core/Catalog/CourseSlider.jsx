1fec1657600c06384faa33aadcdc095e32b2a566
vivek-panchal <vivek.nsut@gmail.com>
2024-03-27 13:46:37 +0530

    image size change

--- src/components/core/Catalog/CourseSlider.jsx
+++ src/components/core/Catalog/CourseSlider.jsx
@@ -22,7 +22,7 @@ const CourseSlider = ({Courses}) => {
           >
             {Courses?.map((course, i) => (
               <SwiperSlide key={i}>
-                <Course_Card course={course} Height={"h-[250px]"} />
+                <Course_Card course={course} Height={"h-[215px]"} />
               </SwiperSlide>
             ))}
           </Swiper>
